e73dda21d419da9fc77df09a7b52daf45e14a441
anyingjinghun <fenghua.jiang@shenmajr.com>
2017-03-21 20:55:29 +0800

    #模块目录

---
+++ cloud-shop-web/src/main/java/com/shop/web/cust/DemoController.java
@@ -0,0 +1,20 @@
+package com.shop.web.cust;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.service.DemoService;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+	
+	@Autowired
+	private DemoService demoService;
+	
+	@RequestMapping("/helloWorld")
+	public String helloWorld() {
+		return demoService.speak();
+	}
+}
